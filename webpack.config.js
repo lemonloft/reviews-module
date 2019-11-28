abb298c49e20a2cee2cc8c4fd1cc1945547985b3
Peter Park <impeter9@gmail.com>
2019-11-27 19:52:28 -0800

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './client/index.jsx',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$|\.m?jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
