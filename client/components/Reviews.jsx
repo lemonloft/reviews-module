17effc62a9c19532ca2b49b7c83c9094e60a081c
Peter Park <impeter9@gmail.com>
2019-12-03 17:09:55 -0800

    implemented the correct date output for reviews

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -5,35 +5,49 @@ const Body = styled.div`
 font-family: circular, Roboto, "Helvetica Neue", sans-serif;
 `;
 
+const Review = styled.div`
+margin-top: 24px; 
+margin-bottom: 24px;
+`;
+
 class Reviews extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    getProperDate(dateString) {
+        var monthNames = [
+            "", "January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"
+        ];
+        var date = new Date(dateString);
+        return monthNames[date.getMonth()] + ' ' + date.getFullYear();
+    }
+
     render() {
+
         return (
             <Body>
                 <div>Reviews</div>
                 {this.props.allState.data.map((data) =>{
                     return (
-                        <div key={data._id}>
+                        <Review key={data._id}>
                             <div>
                                 <a href="/">
                                     <img src={data.image} height="40" width="40"></img>
                                 </a>
                                 {data.name}
-                                <div>{data.date}</div>
+                                <div>{this.getProperDate(data.date)}</div>
                             </div>
-                            <div>margin-top: 24px; margin-bottom: 24px;</div>
                             <div>
                                 <span>
                                     {data.body}
                                 </span>
                                 <button>read more</button>
                             </div>
-                            <div>margin-top: 24px; margin-bottom: 24px;</div>
-                        </div>
-
+                        </Review>
                     );}
                 )}
             </Body>
