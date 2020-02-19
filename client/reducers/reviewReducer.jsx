79361ea92eb1fd0820e099e4c9baa98185adbc30
Peter Park <impeter9@gmail.com>
2020-02-18 21:19:11 -0800

    add payload to reviewReducer trav 44:00

--- client/reducers/reviewReducer.jsx
+++ client/reducers/reviewReducer.jsx
@@ -1,12 +1,18 @@
 import { FETCH_REVIEWS } from '../actions/types.jsx';
 
 const initialState = {
-    items: [],
-    item: {},
+    data: [],
+    staticData: {},
 }
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case FETCH_REVIEWS:
+            return {
+                ...state,
+                data: action.payload[0],
+                staticData: action.payload[1]
+            }
         default:
             return state;
     }
